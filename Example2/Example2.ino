da2f36a38c5f2332b45ac278c4797387dcad41d3
aminedachir <dachiramine110@gmail.com>
2021-06-09 23:52:19 +0100

    blink

--- Example2/Example2.ino
+++ Example2/Example2.ino
@@ -1,9 +1,16 @@
+const int led1 = 7;
+const int led2 = 2;
+int D = 100;
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(led1,1);
+  digitalWrite(led2,0);
+  delay(D);
+  digitalWrite(led1,0);
+  digitalWrite(led2,1);
+  delay(D);
+  D = D + 50; 
 }
