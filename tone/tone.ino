32a97bb2c6e8ca721a49ed1f9920caec689d40de
aminedachir <dachiramine110@gmail.com>
2021-06-10 23:52:32 +0100

    add 3 switches and 1 speaker

--- tone/tone.ino
+++ tone/tone.ino
@@ -1,20 +1,12 @@
-int spk = 2;
-int sw1 = 8;
-int sw2 = 7;
-int sw3 = 4;
-int D1 = 250;
-int D2 = 500;
-int D3 = 750;
+int a = 9;b = 8;c = 7;
 void setup() {
-  pinMode(spk,OUTPUT);
-  pinMode(sw1,INPUT_PULLUP);
-  pinMode(sw2,INPUT_PULLUP);
-  pinMode(sw3,INPUT_PULLUP);
+  pinMode(12,OUTPUT);
+  pinMode(a,INPUT_PULLUP);
+  pinMode(b,INPUT_PULLUP);
+  pinMode(c,INPUT_PULLUP);
 }
 void loop() {
-  int D;
-  if (digitalRead(sw1)== 1){D = D1;}
-  else if (digitalRead(sw2)== 0){D = D2;}
-  else if (digitalRead(sw3)== 0){D = D3;}
-  tone(spk,D,500);
+  if (digitalRead(a)== 1){tone(12,261,500);}
+  if (digitalRead(b)== 0){tone(12,289,500);}
+  if (digitalRead(c)== 0){tone(12,344,500);}
 }
