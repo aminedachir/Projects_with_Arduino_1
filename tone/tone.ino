99d7fa38c319e1bf768cec0032a1f156e8c4e376
aminedachir <dachiramine110@gmail.com>
2021-06-10 23:10:36 +0100

    tone

---
+++ tone/tone.ino
@@ -0,0 +1,13 @@
+int spk = 2;
+int sw = 8;
+int D1 = 250;
+int D2 = 500;
+int D3 = 750;
+void setup() {
+  pinMode(spk,OUTPUT);
+  pinMode(sw,INPUT_PULLUP);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
